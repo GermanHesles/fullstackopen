301c55de8a3a3ac916a98d68b03419caa6275128
GermanHesles <german.hesles.hernandez@gmail.com>
2021-06-14 22:40:43 +0200

    Add map

--- part2/courseinfo2/src/App.js
+++ part2/courseinfo2/src/App.js
@@ -1,41 +1,4 @@
 import './App.css';
-// import Note from './Note.js';
-
-// const notes = [
-//   {
-//     id: 1,
-//     content: 'HTML is easy',
-//     date: '2019-05-30T17:30:31.098Z',
-//     important: true
-//   },
-//   {
-//     id: 2,
-//     content: 'Browser can execute only JavaScript',
-//     date: '2019-05-30T18:39:34.091Z',
-//     important: false
-//   },
-//   {
-//     id: 3,
-//     content: 'GET and POST are the most important methods of HTTP protocol',
-//     date: '2019-05-30T19:20:14.298Z',
-//     important: true
-//   }
-// ]
-
-// function App() {
-//   if (typeof notes === 'undefined' || notes.length === 0) {
-//     return 'No tenemos notas'
-//   }
-
-//   return (
-//     <ol>
-//       {notes.map((note) => (
-//         // <Note key={note.id} id={note.id} content={note.content} date={note.date} />
-//         <Note key={note.id} {...note} />
-//       ))}
-//     </ol>
-//   );
-// }
 
 const Course = (props) => {
   return (
@@ -51,32 +14,33 @@ const Header = (props) => {
   return <h1>{props.course.name}</h1>
 }
 
-const Part = (props) => {
+const Parts = (props) => {
+  console.log(props);
+  const listItems = (part) => {
+    return (
+        <p key={part.id}>{part.name} {part.exercises}</p>
+    )
+  }
+
   return (
-    <p>
-    {props.part} {props.exercises}
-    </p>
+    <div>
+      {props.part.map(listItems)}
+    </div>
   )
 }
 
 const Content = (props) => {
-  return (
-    <div>
-      <Part part={props.parts[0].name} exercises={props.parts[0].exercises} />
-      <Part part={props.parts[1].name} exercises={props.parts[1].exercises} />
-      <Part part={props.parts[2].name} exercises={props.parts[2].exercises} />
-    </div>
-  )
+  return <Parts part={props.parts}/>
 }
 
 const Total =(props) => {
   const total = props.parts.reduce((accumulator, currentValue) => {
     return accumulator + currentValue.exercises
   }, 0)
-  
+
   return (
     <p>Number of exercises {total}</p>
-  ) 
+  )
 }
 
 const App = () => {
@@ -107,7 +71,11 @@ const App = () => {
     ]
   }
 
-  return <Course course={course} />
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
 }
 
 export default App;
