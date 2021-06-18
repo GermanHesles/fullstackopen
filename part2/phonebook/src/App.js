f578036f46bf1c0d0164712ccc1bf47c1a83ca2e
GermanHesles <german.hesles.hernandez@gmail.com>
2021-06-18 19:32:50 +0200

    Add event handler

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,22 +3,44 @@ import React, { useState } from 'react'
 const App = () => {
   const [ persons, setPersons ] = useState([
     { name: 'Arto Hellas' }
-  ]) 
-  const [ newName, setNewName ] = useState('')
+  ]);
+  const [ newName, setNewName ] = useState('');
+
+  const handleChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    console.log('crear persona');
+    const personsToAddToState = {
+      name: newName  
+    };
+    console.log(personsToAddToState);
+
+    setPersons(persons.concat(personsToAddToState));
+    setNewName('');
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
-          name: <input />
+          name: <input type='text' onChange= {handleChange} value={newName}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <div>
+        {
+          persons.map((person) => {
+            return <p>{person.name}</p>
+          })
+        }
+      </div>
     </div>
   )
 }
