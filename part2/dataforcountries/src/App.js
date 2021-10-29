1d7f34415333ce98d2f95ba9c083802d90ea6b41
Germán Hesles <german.hesles.hernandez@gmail.com>
2021-10-29 22:32:48 +0200

    Add country list
    Co-authored-by: Luis Hdez <luis.munoz.hdez@gmail.com>

--- part2/dataforcountries/src/App.js
+++ part2/dataforcountries/src/App.js
@@ -1,8 +1,34 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
+const CountryList = ({countriesFiltered}) => {
+  if(countriesFiltered.length > 10) {
+    return <p>Too many matches</p>
+  } if (countriesFiltered.length === 1) {
+    return countriesFiltered.map((country) => (
+      <div key={country.cca2}> 
+        <h1>{country.name.common}</h1>
+        <p>{country.capital}</p> 
+        <p>population {country.population}</p>
+        <h3>languages</h3>
+        <ul>{Object.values(country.languages).map((language) => {
+          return <li>{language}</li>
+        })}</ul> 
+        <img src={country.flags.png} alt={country.name.official} />
+      </div>
+    ))
+  } else {
+    return countriesFiltered.map((country) => {
+      return <p key={country.cca2}>{country.name.common}</p>
+    })
+  }
+}
+   
+
 function App() {
-  const [all, setAll] = useState([]);
+  const [countries, setCountries] = useState([]);
+  const [search, setSearch] = useState('');
+  const [countriesFiltered, setCountriesFiltered] = useState([]);
 
   useEffect(() => {
     console.log('effect')
@@ -10,14 +36,27 @@ function App() {
       .get('https://restcountries.com/v3.1/all')
       .then(response => {
         console.log('promise fulfilled')
-        setAll(response.data)
+        setCountries(response.data)
       })
   }, [])
 
+  const handleSearch = (event) =>
+  {
+    const finalSearch = event.target.value;
+
+    setSearch(finalSearch);
+    const countriesFiltered = countries.filter((country) => {
+      return country.name.common.toLowerCase().includes(finalSearch.toLowerCase())
+    })
+    setCountriesFiltered(countriesFiltered);
+  }
+
+
   return (
     <div className="App">
-      countries:
-      <input type='text'/>
+      find countries:
+      <input type='text' onChange={handleSearch} value={search}/>
+      <CountryList countriesFiltered={countriesFiltered} />
     </div>
   );
 }
