c1002cd208ab0140b5c3904906d6ee8adb1dffd7
Germán Hesles <german.hesles.hernandez@gmail.com>
2021-11-03 22:01:35 +0100

    Add weather
    Co-authored-by: Luis Hdez <luis.munoz.hdez@gmail.com>

--- part2/dataforcountries/src/App.js
+++ part2/dataforcountries/src/App.js
@@ -2,6 +2,33 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 
+const WeatherCountry = ({country}) => {
+
+  const [weatherCountries, setWeatherCountries] = useState([]);
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get(`http://api.weatherstack.com/current?access_key=a96e48df4164aa3d8f1d2529034d5aa3&query=${country.capital}`)
+      .then(response => {
+        console.log(response.data)
+        setWeatherCountries(response.data)
+      });
+  },[country.capital]);
+
+  if (!weatherCountries.current) {
+    return <div>Loading...</div>
+  }
+
+  return (
+    <div>
+      <h3>Weather in {country.capital}</h3>
+      <b>temperature:</b> {weatherCountries.current.temperature}<br />
+      <img src={weatherCountries.current.weather_icons[0]} alt={weatherCountries.current.weather_descriptions[0]} /><br />
+      <b>wind:</b> {weatherCountries.current.wind_speed}
+    </div>
+  )
+}
 
 const Languages = ({languages}) => {
   return (
@@ -11,7 +38,7 @@ const Languages = ({languages}) => {
   )
 }
 
-const MatchedCountry = ({countriesFiltered}) => {
+const MatchedCountry = ({countriesFiltered, setWeatherCountries}) => {
   return countriesFiltered.map((country) => (
     <div key={country.cca2}> 
       <h1>{country.name.common}</h1>
@@ -20,6 +47,7 @@ const MatchedCountry = ({countriesFiltered}) => {
       <h3>languages</h3>
       <Languages languages={country.languages} /> 
       <img src={country.flags.png} alt={country.name.official} />
+      <WeatherCountry setWeatherCountries={setWeatherCountries} country={country}/>
     </div>
   ))
 }
@@ -28,7 +56,7 @@ function App() {
   const [countries, setCountries] = useState([]);
   const [search, setSearch] = useState('');
   const [countriesFiltered, setCountriesFiltered] = useState([]);
-
+  
   useEffect(() => {
     console.log('effect')
     axios
@@ -36,32 +64,32 @@ function App() {
       .then(response => {
         console.log('promise fulfilled')
         setCountries(response.data)
-      })
-  }, [])
+      });
+  }, []);
 
   const filterSearch = (searchedCountry) => {
     const countriesFiltered = countries.filter((country) => {
       return country.name.common.toLowerCase().includes(searchedCountry.toLowerCase())
-    })
+    });
 
     setCountriesFiltered(countriesFiltered);
-  }
+  };
 
   const handleSearch = (event) =>
   {
     const country = event.target.value;
     filterSearch(country)
     setSearch(country);
-  }
+  };
 
   const CountryList = ({countriesFiltered}) => {
     if (countriesFiltered.length > 10) {
       return <p>Too many matches</p>
-    }
+    };
   
     if (countriesFiltered.length === 1) {
       return <MatchedCountry countriesFiltered={countriesFiltered} />
-    }
+    };
   
     return countriesFiltered.map((country) => {
       return (
@@ -69,8 +97,8 @@ function App() {
           {country.name.common} <button name='button' onClick={() => {filterSearch(country.name.common)}}>show</button>
         </p>
       )
-    })
-  }
+    });
+  };
 
   return (
     <div className="App">
@@ -79,6 +107,6 @@ function App() {
       <CountryList countriesFiltered={countriesFiltered} />
     </div>
   );
-}
+};
 
 export default App;
